bfbb300aef4382d039e0a7d25cc3bb151e7718da
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2024-04-11 17:00:04 +0200

    One dimensional array

---
+++ C/Arrays and Strings/1D_Array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n, total = 0;
+    scanf("%d", &n);
+    int *arr = (int*)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; ++i)
+        scanf("%d", &arr[i]);
+
+    for (int i = 0; i < n; ++i)
+        total += arr[i];
+
+    printf("%d\n", total);
+    return 0;
+
+}
