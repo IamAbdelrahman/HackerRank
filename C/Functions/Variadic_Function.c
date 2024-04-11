10e9cbdd4261d9ae5fcd4258189e4f0c42807fd2
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2024-04-11 17:22:25 +0200

    Variadic functions

---
+++ C/Functions/Variadic_Function.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int  sum (int count,...) {
+  int sum = 0;
+  va_list ptr;
+  va_start(ptr, count);
+  for (int i = 0; i < count; i++)
+  {
+    sum += va_arg(ptr, int);
+  }
+  va_end(ptr);
+  return sum; 
+}
+
+int min(int count,...) {
+  va_list ptr;
+  va_start(ptr, count);
+  int min = va_arg(ptr, int);
+  for (int i = 0; i < count; i++)
+  {
+    int temp = va_arg(ptr, int);
+    min = temp < min ? temp : min;
+  }
+  va_end(ptr);
+  return min;
+}
+
+int max(int count,...) {
+  va_list ptr;
+  va_start(ptr, count);
+  int max = va_arg(ptr, int);
+  for (int i = 0; i < count; i++)
+  {
+    int temp = va_arg(ptr, int);
+    max = temp > max ? temp : max;
+  }
+  va_end(ptr);
+  return max;
+}
+
